2666b9868a9ea70c8ec6c062293e94154f6dfd7e
Matt Ross <matt@tronik.co.uk>
2016-03-08 17:07:20 +0000

    Created empty MVC app
    Selected to host on Azure. Went for MVC 4 over Core.

---
+++ src/ElevatorSharp.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ElevatorSharp.Web.MvcApplication" Language="C#" %>
---
+++ src/ElevatorSharp.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ElevatorSharp.Web.Startup))]
+namespace ElevatorSharp.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
