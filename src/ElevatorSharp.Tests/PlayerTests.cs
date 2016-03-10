77798555d8279d12e37f581058d935f03d314552
Matt Ross <matt@tronik.co.uk>
2016-03-10 19:09:47 +0000

    Init_takes_IEnumerable_of_Elevator - green
    Another unit test for Player.

--- src/ElevatorSharp.Tests/PlayerTests.cs
+++ src/ElevatorSharp.Tests/PlayerTests.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
 
 namespace ElevatorSharp.Tests
 {
@@ -11,12 +13,24 @@ namespace ElevatorSharp.Tests
             IPlayer player = new TestPlayer();
             Assert.IsNotNull(player.GetType().GetMethod("Init"));
         }
+
+        [Test]
+        public void Init_takes_IEnumerable_of_Elevator()
+        {
+            IPlayer player = new TestPlayer();
+            var parameters = player.GetType().GetMethod("Init").GetParameters();
+            Assert.IsTrue(parameters.Any(p => p.ParameterType == typeof(IEnumerable<Elevator>)));
+        }
          
+    }
+
+    public class Elevator
+    {
     }
 
     public class TestPlayer : IPlayer
     {
-        public void Init()
+        public void Init(IEnumerable<Elevator> elevators)
         {
             throw new System.NotImplementedException();
         }
@@ -24,6 +38,6 @@ namespace ElevatorSharp.Tests
 
     public interface IPlayer
     {
-        void Init();
+        void Init(IEnumerable<Elevator> elevators);
     }
 }
