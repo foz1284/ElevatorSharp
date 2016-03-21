0b967567272e8ed65f695af8abb962389f867ebc
Matt Ross <matt@tronik.co.uk>
2016-03-21 18:19:29 +0000

    Added endpoint for Floor DownButtonPressed.

--- src/ElevatorSharp.Web/Controllers/ElevatorController.cs
+++ src/ElevatorSharp.Web/Controllers/ElevatorController.cs
@@ -83,10 +83,24 @@ namespace ElevatorSharp.Web.Controllers
         /// <summary>
         /// Triggered when someone has pressed the up button at a floor. 
         /// Note that passengers will press the button again if they fail to enter an elevator.
+        /// Maybe tell an elevator to go to this floor?
         /// </summary>
         /// <param name="viewModel"></param>
         /// <returns></returns>
         public ContentResult UpButtonPressed(UpButtonPressedViewModel viewModel)
+        {
+            var json = JsonConvert.SerializeObject(viewModel);
+            return Content(json, "application/json");
+        }
+
+        /// <summary>
+        /// Triggered when someone has pressed the down button at a floor. 
+        /// Note that passengers will press the button again if they fail to enter an elevator.
+        /// Maybe tell an elevator to go to this floor?
+        /// </summary>
+        /// <param name="viewModel"></param>
+        /// <returns></returns>
+        public ContentResult DownButtonPressed(DownButtonPressedViewModel viewModel)
         {
             var json = JsonConvert.SerializeObject(viewModel);
             return Content(json, "application/json");
