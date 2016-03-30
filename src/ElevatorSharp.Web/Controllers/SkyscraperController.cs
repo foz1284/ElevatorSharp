5c34912921094fb2e983fdc466630a86031358e2
Matt Ross <matt@tronik.co.uk>
2016-03-30 19:39:08 +0100

    Made a start on adding async endpoints.
    Still need to bring the diagnostics stuff from index into Compile
    endpoint. And extract diagnostics messages in callback and show in
    codestatus.

--- src/ElevatorSharp.Web/Controllers/SkyscraperController.cs
+++ src/ElevatorSharp.Web/Controllers/SkyscraperController.cs
@@ -101,6 +101,22 @@ namespace ElevatorSharp.Default
             return RedirectToAction("Index", new {message= "No player implementing IPlayer found." });
         }
 
+        private ContentResult FindPlayerAsync(Assembly playerAssembly, string source = null)
+        {
+            string json;
+            foreach (var type in playerAssembly.GetTypes().OrderBy(t => t.Name))
+            {
+                if (type.GetInterface("IPlayer") == null) continue;
+                var player = Activator.CreateInstance(type) as IPlayer;
+                SavePlayer(player);
+                var message = type.Name + " uploaded.";
+                json = new { message, source }.ToString();
+                return Content(json, "application/json");
+            }
+            json = new { message = "No player implementing IPlayer found." }.ToString();
+            return Content(json, "application/json");
+        }
+
         [ValidateInput(false)]
         public ActionResult UploadPlayerAsCode(string source)
         {
@@ -137,6 +153,47 @@ namespace ElevatorSharp.Default
                 var diagnosticsGuid = Guid.NewGuid().ToString();
                 MemoryCache.Default.Add(diagnosticsGuid, result.Diagnostics, new CacheItemPolicy { AbsoluteExpiration = DateTimeOffset.Now.AddDays(1) });
                 return RedirectToAction("Index", new { message = "Compilation issues", source = sourceGuid, diagnostics = diagnosticsGuid });
+            }
+        }
+
+        [ValidateInput(false)]
+        public ContentResult Compile(string source)
+        {
+            var syntaxTree = CSharpSyntaxTree.ParseText(source);
+            var referenceNames = Regex.Matches(source, "using ([^;]+)").Cast<Match>().Select(m => m.Groups[1].Value);
+            var references = new List<MetadataReference>
+            {
+                MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
+                MetadataReference.CreateFromFile(Assembly.LoadWithPartialName("System.Core").Location)
+            };
+            foreach (var reference in referenceNames)
+            {
+                try
+                {
+                    references.Add(MetadataReference.CreateFromFile(Assembly.LoadWithPartialName(reference).Location));
+                }
+                catch (NullReferenceException)
+                {
+                }
+            }
+            var compilation = CSharpCompilation.Create("IPlayer", new[] { syntaxTree }, references, new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+            using (var ms = new MemoryStream())
+            {
+                var result = compilation.Emit(ms);
+                var sourceGuid = Guid.NewGuid().ToString();
+                MemoryCache.Default.Add(sourceGuid, source, new CacheItemPolicy { AbsoluteExpiration = DateTimeOffset.Now.AddDays(1) });
+                if (result.Success)
+                {
+                    ms.Seek(0, SeekOrigin.Begin);
+                    var dll = Assembly.Load(ms.ToArray());
+                    return FindPlayerAsync(dll, sourceGuid);
+                }
+                var diagnosticsGuid = Guid.NewGuid().ToString();
+                MemoryCache.Default.Add(diagnosticsGuid, result.Diagnostics, new CacheItemPolicy { AbsoluteExpiration = DateTimeOffset.Now.AddDays(1) });
+                var json = "";
+                return Content(json, "application/json");
+                //return RedirectToAction("Index", new { message = "Compilation issues", source = sourceGuid, diagnostics = diagnosticsGuid });
             }
         }
 
