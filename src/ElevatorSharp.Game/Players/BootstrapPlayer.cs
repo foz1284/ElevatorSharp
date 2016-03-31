835e1075902505cee4e9982a0a1cff05c8dd6a60
Matt Ross <matt@tronik.co.uk>
2016-03-31 14:24:38 +0100

    Added BootstrapPlayer.

---
+++ src/ElevatorSharp.Game/Players/BootstrapPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ElevatorSharp.Game.Players
+{
+    public class BootstrapPlayer : IPlayer
+    {
+        public void Init(IElevator[] elevators, IFloor[] floors)
+        {
+            // Let's use the first elevator
+            var elevator = elevators[0];
+
+            // Whenever the elevator is idle (has no more queued destinations) ...
+            elevator.Idle += ElevatorOnIdle;
+        }
+
+        private void ElevatorOnIdle(object sender, EventArgs eventArgs)
+        {
+            var elevator = (IElevator)sender;
+
+            // let's go to all the floors (or did we forget one?)
+            elevator.GoToFloor(0);
+            elevator.GoToFloor(1);
+        }
+    }
+}
